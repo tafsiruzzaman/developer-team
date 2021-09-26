d3a51348efdda0366e0afa81b10edbb7e7278fc7
tafsiruzzaman <tafsirjaman100@gmail.com>
2021-09-26 23:23:49 +0600

    last commit

--- src/components/Added/Added.js
+++ src/components/Added/Added.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const Added = (props) => {
+    const {name, img} = props;
     return (
         <div className="d-flex align-items-center mb-3">
-            <img className='w-25 rounded-circle me-3' src={props.img} alt="" />
-            <h3>{props.name}</h3>
+            <img className='w-25 rounded-circle me-3' src={img} alt="" />
+            <h3>{name}</h3>
         </div>
     );
 };
