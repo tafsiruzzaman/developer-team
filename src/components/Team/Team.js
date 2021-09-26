94dab4afb1d147d695bd0db9ac8ba9f4ac48a112
tafsiruzzaman <tafsirjaman100@gmail.com>
2021-09-26 22:21:17 +0600

    readme file edited

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -8,9 +8,9 @@ const Team = (props) => {
         cost = cost + member.salary;
     }
     return (
-        <div className="ms-4">
+        <div className="ms-3">
             <h2>Member Added: {props.team.length}</h2>
-            <h3>Total cost: {cost}</h3> <br />
+            <h3>Total cost: $ {cost}</h3> <br />
             <h3>Team</h3>
             {
                 team.map(member => <Added key={member.key} name={member.name} img={member.img}></Added>)
